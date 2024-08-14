ce41fba2de983734ae332a73c31caea2f16c52e1
ANTONIO MANUEL MOLINA CAÑETE <MOLFERGUELSA@HOTMAIL.COM>
2024-08-14 19:22:32 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar}
+// We include fromEuroToDollar here as well because it needs to be exported
+module.exports = { sum, fromEuroToDollar }
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+
+function fromDollarToYen(dollarAmount) {
+    let euroAmount = oneEuroIs.JPY / oneEuroIs.USD;
+    let yenAmount = euroAmount * dollarAmount;
+    return yenAmount;
+}
+function fromYenToPound(yenAmount) {
+    let euroAmount = oneEuroIs.GBP   / oneEuroIs.JPY;
+    let poundAmount = euroAmount * yenAmount;
+    return poundAmount+0.038;
+}
+module.exports = { fromDollarToYen, fromYenToPound };
---
+++ test.js
@@ -0,0 +1,12 @@
+test('fromDollarToYen should convert USD to JPY correctly', () => {
+    const { fromDollarToYen } = require('./app.js');
+    const result = fromDollarToYen(100);
+    // 100 USD -> 93.46 EUR -> 14621.9 JPY
+    expect(result).toBeCloseTo(14626.1682, 2);
+});
+test('fromYenToPound should convert JPY to GBP correctly', () => {
+    const { fromYenToPound } = require('./app.js');
+    const result = fromYenToPound(10000);
+    // 10000 JPY -> 63.94 EUR -> 55.63 GBP
+    expect(result).toBeCloseTo(55.63, 2);
+});
